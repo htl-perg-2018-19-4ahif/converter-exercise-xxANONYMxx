044e19e9256e74069a9e38374ff3d8cd21790281
Samuel Lange <samuelmanuel.lange@gmail.com>
2018-10-24 15:15:42 +0200

    Finished

--- convert.js
+++ convert.js
@@ -1,1 +1,7 @@
-console.log('Not implemented yet!');
+const convert = require('convert-units');
+if(isNaN(process.argv[2]) || process.argv[4] != "to"){
+    console.log('Error');
+}
+else{
+    console.log(convert(process.argv[2]).from(process.argv[3]).to(process.argv[5]));
+}
