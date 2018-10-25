354b8bd0203ba6a711932186c7b511ad4fb19dc2
Samuel Lange <samuelmanuel.lange@gmail.com>
2018-10-25 13:36:23 +0200

    not working properly

--- convert.js
+++ convert.js
@@ -3,5 +3,5 @@ if(isNaN(process.argv[2]) || process.argv[4] != "to"){
     console.log('Error');
 }
 else{
-    console.log(convert(process.argv[2]).from(process.argv[3]).to(process.argv[5]));
+    console.log(process.argv[2] + " " + process.argv[3] + " are " + convert(process.argv[2]).from(process.argv[3]).to(process.argv[5] + process.argv[5]));
 }
